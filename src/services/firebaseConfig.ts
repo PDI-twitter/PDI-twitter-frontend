154129e82c575f288ac1157bef9a4326168517cb
Lucas Schimith Cosendey <33984977+Schimith98@users.noreply.github.com>
2023-10-05 18:16:58 -0300

    dotEnv

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjTsHSRIz8WTli81OebIDmQWRqphZjVBs",
-  authDomain: "bench-9f846.firebaseapp.com",
-  projectId: "bench-9f846",
-  storageBucket: "bench-9f846.appspot.com",
-  messagingSenderId: "444580656686",
-  appId: "1:444580656686:web:563227552760531035d1bf",
-  measurementId: "G-V86NN4WRE6",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
